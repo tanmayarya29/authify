862447be7501ce0d9039198ffea0fc5d76057152
Tanmay Mishra <tanmayarya29@gmail.com>
2023-08-31 10:49:42 +0000

    revert

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { store } from "./redux/app/store.ts";
-// import App from "./App.tsx";
+import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routes/router";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <App /> */}
-      <RouterProvider router={router} />
+      <App />
     </Provider>
   </React.StrictMode>
 );
