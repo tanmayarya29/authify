646fbe71d5563a1cda6699041fb7a539f05a13bb
Tanmay Mishra <tanmayarya29@gmail.com>
2023-09-01 20:42:35 +0530

    added title

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { store } from "./redux/app/store.ts";
-import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
@@ -9,9 +8,8 @@ import { router } from "./routes/router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
